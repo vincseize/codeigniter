4699147f6f48c4e1f549b786d80536dbfcc741d1
vincseize <vincseize@gmail.com>
2018-02-12 01:01:58 +0100

    Add files via upload

---
+++ assets/blog/modal.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once('../../application/config/config.php'); 
+
+
+
+// echo HOST;
+
+// require_once('../../libraries/Blog_dbController.php'); 
+
+// echo base_url();
+
+				// $sql = "SELECT COUNT(*) 'total_rows' FROM `".TABLE_FRONTEND."`";
+				// $res = mysqli_fetch_object(mysqli_query($conn, $sql));
+				// $total_rows = $res->total_rows;
+
+	// require_once(base_url().'libraries/Blog_dbController.php'); 
+	// $c = new Blog_dbController();
+	// $conn = $c->connectDB();
+
+
+// $sql = mysqli_query("SELECT * FROM `articles` WHERE id=`1`");
+// $rows = array();
+// while($r = mysqli_fetch_assoc($sql)) {
+//     $rows[] = $r;
+// }
+// print json_encode($rows);
+
+?>
